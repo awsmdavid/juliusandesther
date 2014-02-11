7b696e40f2e9f38ed5b31cb49292899f54b8e568
awsmdavid <awsmdavid@gmail.com>
2014-02-11 16:18:03 -0500

    added crappy, crappy security

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,31 +1,14 @@
-$(function(){
-    $('.fadein img:gt(0)').hide();
-    $('#photos-leftarrow').click(function(){
-        $('.fadein :first-child').fadeOut();
-        $('.fadein :last-child').fadeIn();
-        $('.fadein :last-child').prependTo('.fadein');
-        });
-    $('#photos-rightarrow').click(function(){$('.fadein :first-child').fadeOut()
-        .next('img').fadeIn()
-        .end().appendTo('.fadein');
-        });
-});
-
-function goToByScroll(id){
-    $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=150},{duration: 1500, easing: "easeInOutExpo"});
-}
-
 $(window).load(function () {
     $('#mask').fadeOut('slow');
 });
 
-
-var thePassword = "craps";
-password=prompt('Enter Password','');
-password=md5(password);
-if(md5(password) == thePassword){ window.location = password + "/"; }
-else { alert("incorrect"); }
-
-
-
-
+function login() {
+    var thePassword = "craps";
+    var password=prompt('Enter Password','');
+    if (password==thePassword){
+        window.location = "secure.html";
+    }
+    else {
+        alert("incorrect password =(");
+    }
+}
---
+++ resources/js/secure.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    $('#photos-leftarrow').click(function(){
+        $('.fadein :first-child').fadeOut();
+        $('.fadein :last-child').fadeIn();
+        $('.fadein :last-child').prependTo('.fadein');
+        });
+    $('#photos-rightarrow').click(function(){$('.fadein :first-child').fadeOut()
+        .next('img').fadeIn()
+        .end().appendTo('.fadein');
+        });
+});
+
+function goToByScroll(id){
+    $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=150},{duration: 1500, easing: "easeInOutExpo"});
+}
+
+$(window).load(function () {
+    $('#mask').fadeOut('slow');
+});
+
+
+
+
+
+
