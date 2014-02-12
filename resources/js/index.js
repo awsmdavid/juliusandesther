ea3787cb8e7031f3d553841d7723eba916078260
awsmdavid <awsmdavid@gmail.com>
2014-02-12 01:38:15 -0500

    need to update home to change login methods to goto after correct password

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,7 +2,11 @@ $(window).load(function () {
     $('#mask').fadeOut('slow');
 });
 
-function login() {
+function goToByScroll(id){
+    $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=150},{duration: 1500, easing: "easeInOutExpo"});
+}
+
+function login(id) {
     // use to generate future pws
     // var thePassword = "craps";
     // var hashword = CryptoJS.MD5("craps");
@@ -11,9 +15,28 @@ function login() {
     var theHashword = "de59daa1a72b6c1155b05eef4a431ec4";
     var password=prompt('Enter Password','');
     if (CryptoJS.MD5(password+"2aecc21ce57c973d624175017c3f4616")==theHashword){
-        window.location = "secure"+CryptoJS.MD5(password)+".html";
+        // window.location = "secure"+CryptoJS.MD5(password)+".html";
+        var hashlink = "secure"+CryptoJS.MD5(password)+".html";
+        $('#secure-content').load(hashlink + ' #secure-content');
+        $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=150},{duration: 1500, easing: "easeInOutExpo"});
+
     }
     else {
         alert("incorrect password =(");
     }
 }
+
+
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    $('#photos-leftarrow').click(function(){
+        $('.fadein :first-child').fadeOut();
+        $('.fadein :last-child').fadeIn();
+        $('.fadein :last-child').prependTo('.fadein');
+        });
+    $('#photos-rightarrow').click(function(){$('.fadein :first-child').fadeOut()
+        .next('img').fadeIn()
+        .end().appendTo('.fadein');
+        });
+});
