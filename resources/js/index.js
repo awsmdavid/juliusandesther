9fa441c3c8432aca9041dd875aea817f959abb7b
awsmdavid <awsmdavid@gmail.com>
2014-02-12 00:37:06 -0500

    sick hashword

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,10 +3,15 @@ $(window).load(function () {
 });
 
 function login() {
-    var thePassword = "craps";
+    // use to generate future pws
+    // var thePassword = "craps";
+    // var hashword = CryptoJS.MD5("craps");
+    // var hash = CryptoJS.MD5("craps"+"2aecc21ce57c973d624175017c3f4616");
+
+    var theHashword = "de59daa1a72b6c1155b05eef4a431ec4";
     var password=prompt('Enter Password','');
-    if (password==thePassword){
-        window.location = "secure.html";
+    if (CryptoJS.MD5(password+"2aecc21ce57c973d624175017c3f4616")==theHashword){
+        window.location = "secure"+CryptoJS.MD5(password)+".html";
     }
     else {
         alert("incorrect password =(");
